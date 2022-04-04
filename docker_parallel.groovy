912ae47acb2aa257917a281edf62b8fe5a044b78
Iyadomry <iyad2fast@hotmail.com>
2022-04-04 22:52:32 +0300

    Update docker_parallel.groovy

--- docker_parallel.groovy
+++ docker_parallel.groovy
@@ -10,32 +10,11 @@ def docker1() {
             stage('remove all running containers') {
                 sh 'docker rm -f $(docker ps -a -q)'
                 }
-            }
+        }
         catch (Exception) {
             echo ' There is no current containers to delete, try again '
             currentBuild.result = 'SUCCESS'
-            }
-
-
-            // def tasks = [:]
-            // tasks["task_1"] = {
-            //     stage ("task_1"){    
-            //         node('jenkins') {  
-            //             sh  "docker run --name ${name1} -d --rm -p ${port1}:80  nginx"
-            //             sleep 30
-            //         }
-            //     }
-            // }
-            // tasks["task_2"] = {
-            //     stage ("task_2"){    
-            //         node('jenkins') {  
-            //             sh "docker run --name ${name2} -d --rm -p ${port2}:80  nginx"
-            //             sleep 30
-            //         }
-            //     }
-            // }
-            // parallel tasks
-            
+        }
             parallel(
                 "StageA": {
                     stage("Deploy ${name1} ") {
@@ -63,14 +42,3 @@ def docker1() {
 }
 
 return this
-
-
-// def stage = [:]
-// stage("stage1") = {
-//     sh "docker run --name ${name1} -d --rm -p ${port1}:80  nginx"
-// }
-// stage("stage2") = {
-//     sh "docker run --name ${name2} -d --rm -p ${port2}:80  nginx"
-// }
-
-// parallel stage
