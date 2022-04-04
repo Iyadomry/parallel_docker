15e34331f1e892550ff3e3becbda3161dcec641b
Iyadomry <iyad2fast@hotmail.com>
2022-04-04 21:26:23 +0300

    Update docker_parallel.groovy

---
+++ docker_parallel.groovy
@@ -0,0 +1,36 @@
+def main() {
+    ansiColor('xterm') {
+        docker1() 
+    }
+}
+
+def docker1() {
+    node(label: 'jenkins') {
+        properties(
+        [parameters([string(defaultValue: '58', name: 'port1'), string(defaultValue: 'app3', name: 'name1' )])]
+        [parameters([string(defaultValue: '59', name: 'port2'), string(defaultValue: 'app4', name: 'name2' )])]
+        )
+        try {
+            stage('remove all running containers') {
+                sh 'docker rm -f $(docker ps -a -q)'
+                }
+            }
+        catch (Exception) {
+            echo ' There is no current containers to delete, try again '
+            currentBuild.result = 'SUCCESS'
+            }
+
+        stage('Deply my application as a container') {
+            ansiColor('xterm') {
+                echo "\u001B[31mbuild new container\u001B[0m"
+                sh "docker run --name ${name} -d --rm -p ${port}:80  nginx"
+            }
+        }
+        stage(name: 'check if the container os running') {
+            sh 'docker ps -a'
+            sh "curl localhost:${port} "
+        }
+    }
+}
+
+return this
